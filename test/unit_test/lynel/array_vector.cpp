ef2cc2b55550a2dd7a747daf83816fcdd7d2c899
Timothy Lyons <t.brandon.lyons@gmail.com>
2020-10-08 20:20:44 +0000

    Use libvodka array
    tybl/containers was subsumed by libvodka.
    
    Relevant Links:
    - Issue 5: Use tybl/containers (https://github.com/tybl/lynel/issues/5)

--- test/unit_test/lynel/array_vector.cpp
+++ test/unit_test/lynel/array_vector.cpp
@@ -1,6 +1,6 @@
 // License: The Unlicense (https://unlicense.org)
 
-#include "tybl/containers/array.hpp"
+#include "vodka/array/array.hpp"
 
 #include <algorithm>
 #include <cmath>
@@ -10,64 +10,64 @@
 namespace tybl::lynel::std::array_vector {
 
 template <typename T, size_t M>
-auto operator+(tybl::containers::array<T,M> const& l, tybl::containers::array<T,M> const& r) -> tybl::containers::array<T,M> {
-  tybl::containers::array<T,M> result;
+auto operator+(tybl::vodka::array::array<T,M> const& l, tybl::vodka::array::array<T,M> const& r) -> tybl::vodka::array::array<T,M> {
+  tybl::vodka::array::array<T,M> result;
   ::std::transform(l.begin(), l.end(), r.begin(), result.begin(), ::std::plus());
   return result;
 }
 
 template <typename T, size_t M>
-auto operator-(tybl::containers::array<T,M> const& l, tybl::containers::array<T,M> const& r) -> tybl::containers::array<T,M> {
-  tybl::containers::array<T,M> result;
+auto operator-(tybl::vodka::array::array<T,M> const& l, tybl::vodka::array::array<T,M> const& r) -> tybl::vodka::array::array<T,M> {
+  tybl::vodka::array::array<T,M> result;
   ::std::transform(l.begin(), l.end(), r.begin(), result.begin(), ::std::minus());
   return result;
 }
 
 template <typename T, size_t M>
-auto operator*(T l, tybl::containers::array<T,M> r) -> tybl::containers::array<T,M> {
+auto operator*(T l, tybl::vodka::array::array<T,M> r) -> tybl::vodka::array::array<T,M> {
   for (auto& e : r) { e *= l; }
   return r;
 }
 
 template <typename T, size_t M>
-constexpr auto operator*(tybl::containers::array<T,M> const& l, T r) -> tybl::containers::array<T,M> {
+constexpr auto operator*(tybl::vodka::array::array<T,M> const& l, T r) -> tybl::vodka::array::array<T,M> {
   return r * l;
 }
 
 template <typename T, size_t M>
-auto operator-(tybl::containers::array<T,M> const& v) -> tybl::containers::array<T,M> {
+auto operator-(tybl::vodka::array::array<T,M> const& v) -> tybl::vodka::array::array<T,M> {
   return static_cast<T>(-1) * v;
 }
 
 template <typename T, size_t M>
-constexpr auto dot_product(tybl::containers::array<T,M> const& l, tybl::containers::array<T,M> const& r) -> T {
+constexpr auto dot_product(tybl::vodka::array::array<T,M> const& l, tybl::vodka::array::array<T,M> const& r) -> T {
   return ::std::inner_product(l.begin(), l.end(), r.begin(), 0.0);
 }
 
 template <typename T, size_t M>
-constexpr auto magnitude(tybl::containers::array<T,M> const& v) -> T {
+constexpr auto magnitude(tybl::vodka::array::array<T,M> const& v) -> T {
   return ::std::sqrt(dot_product(v,v));
 }
 
 template <typename T>
-constexpr auto cross_product(tybl::containers::array<T,3> const& l, tybl::containers::array<T,3> const& r) -> tybl::containers::array<T,3> {
-  return tybl::containers::array<T,3>{ l[1]*r[2] - l[2]*r[1],
+constexpr auto cross_product(tybl::vodka::array::array<T,3> const& l, tybl::vodka::array::array<T,3> const& r) -> tybl::vodka::array::array<T,3> {
+  return tybl::vodka::array::array<T,3>{ l[1]*r[2] - l[2]*r[1],
                             l[2]*r[0] - l[0]*r[2],
                             l[0]*r[1] - l[1]*r[0] };
 }
 
 template <typename T, size_t M>
-constexpr auto projection(tybl::containers::array<T,M> const& l, tybl::containers::array<T,M> const& r) -> tybl::containers::array<T,M> {
+constexpr auto projection(tybl::vodka::array::array<T,M> const& l, tybl::vodka::array::array<T,M> const& r) -> tybl::vodka::array::array<T,M> {
   return (dot_product(l, r) / dot_product(r, r)) * r;
 }
 
 template <typename T, size_t M>
-constexpr auto rejection(tybl::containers::array<T,M> const& l, tybl::containers::array<T,M> const& r) -> tybl::containers::array<T,M> {
+constexpr auto rejection(tybl::vodka::array::array<T,M> const& l, tybl::vodka::array::array<T,M> const& r) -> tybl::vodka::array::array<T,M> {
   return l - projection(l,r);
 }
 
 template <typename T, size_t M>
-auto operator<<(::std::ostream& out, tybl::containers::array<T,M> const r) -> ::std::ostream& {
+auto operator<<(::std::ostream& out, tybl::vodka::array::array<T,M> const r) -> ::std::ostream& {
   for (auto x : r) { out << x << " "; }
   return out;
 }
@@ -79,9 +79,9 @@ using namespace tybl::lynel::std::array_vector;
 #include <doctest/doctest.h>
 
 TEST_CASE("Associative law for vector addition") {
-  tybl::containers::array<double,3> a{ 1, 2, 3 };
-  tybl::containers::array<double,3> b{ 4, 5, 6 };
-  tybl::containers::array<double,3> c{ 7, 8, 9 };
+  tybl::vodka::array::array<double,3> a{ 1, 2, 3 };
+  tybl::vodka::array::array<double,3> b{ 4, 5, 6 };
+  tybl::vodka::array::array<double,3> c{ 7, 8, 9 };
 
   auto l = (a + b)+ c;
   auto r =  a +(b + c);
@@ -90,8 +90,8 @@ TEST_CASE("Associative law for vector addition") {
 }
 
 TEST_CASE("Commutative law for vector addition") {
-  tybl::containers::array<double,3> a{ 1, 2, 3 };
-  tybl::containers::array<double,3> b{ 4, 5, 6 };
+  tybl::vodka::array::array<double,3> a{ 1, 2, 3 };
+  tybl::vodka::array::array<double,3> b{ 4, 5, 6 };
 
   auto l = a + b;
   auto r = b + a;
@@ -100,7 +100,7 @@ TEST_CASE("Commutative law for vector addition") {
 }
 
 TEST_CASE("Associate law for scalar-vector multiplication") {
-  tybl::containers::array<double,3> a{ 1, 2, 3 };
+  tybl::vodka::array::array<double,3> a{ 1, 2, 3 };
   double s = 5.0;
   double t = 3.0;
 
@@ -111,8 +111,8 @@ TEST_CASE("Associate law for scalar-vector multiplication") {
 }
 
 TEST_CASE("Commutative law for scalar-vector multiplication") {
-  tybl::containers::array<double,3> a{ 1, 2, 3 };
-  tybl::containers::array<double,3> b{ 4, 5, 6 };
+  tybl::vodka::array::array<double,3> a{ 1, 2, 3 };
+  tybl::vodka::array::array<double,3> b{ 4, 5, 6 };
   double t = 9.0;
 
   auto l = t * (a + b);
@@ -122,9 +122,9 @@ TEST_CASE("Commutative law for scalar-vector multiplication") {
 }
 
 TEST_CASE("dot product") {
-  tybl::containers::array<double,3> a{ 1, 0, 0 };
-  tybl::containers::array<double,3> b{ 0, 2, 0 };
-  tybl::containers::array<double,3> c{ 2, 2, 2 };
+  tybl::vodka::array::array<double,3> a{ 1, 0, 0 };
+  tybl::vodka::array::array<double,3> b{ 0, 2, 0 };
+  tybl::vodka::array::array<double,3> c{ 2, 2, 2 };
 
   CHECK(1.0 == dot_product(a,a));
   CHECK(0.0 == dot_product(a,b));
@@ -133,8 +133,8 @@ TEST_CASE("dot product") {
 }
 
 TEST_CASE("Commutative law for the dot product") {
-  tybl::containers::array<double,3> a{ 1, 2, 3 };
-  tybl::containers::array<double,3> b{ 4, 5, 6 };
+  tybl::vodka::array::array<double,3> a{ 1, 2, 3 };
+  tybl::vodka::array::array<double,3> b{ 4, 5, 6 };
 
   auto l = dot_product(a, b);
   auto r = dot_product(b, a);
@@ -143,9 +143,9 @@ TEST_CASE("Commutative law for the dot product") {
 }
 
 TEST_CASE("Distributive law for the dot product") {
-  tybl::containers::array<double,3> a{ 1, 2, 3 };
-  tybl::containers::array<double,3> b{ 4, 5, 6 };
-  tybl::containers::array<double,3> c{ 7, 8, 9 };
+  tybl::vodka::array::array<double,3> a{ 1, 2, 3 };
+  tybl::vodka::array::array<double,3> b{ 4, 5, 6 };
+  tybl::vodka::array::array<double,3> c{ 7, 8, 9 };
 
   auto l = dot_product(a, (b + c));
   auto r = dot_product(a, b) + dot_product(a, c);
@@ -154,8 +154,8 @@ TEST_CASE("Distributive law for the dot product") {
 }
 
 TEST_CASE("Scalar factorization for the dot product") {
-  tybl::containers::array<double,3> a{ 1, 2, 3 };
-  tybl::containers::array<double,3> b{ 4, 5, 6 };
+  tybl::vodka::array::array<double,3> a{ 1, 2, 3 };
+  tybl::vodka::array::array<double,3> b{ 4, 5, 6 };
   double t = 9.0;
 
   auto l = dot_product(t * a, b);
@@ -167,9 +167,9 @@ TEST_CASE("Scalar factorization for the dot product") {
 }
 
 TEST_CASE("magnitude") {
-  tybl::containers::array<double,3> a{ 3, 4, 0 };
-  tybl::containers::array<double,3> i{ 1, 0, 0 };
-  tybl::containers::array<double,3> z{ 0, 0, 0 };
+  tybl::vodka::array::array<double,3> a{ 3, 4, 0 };
+  tybl::vodka::array::array<double,3> i{ 1, 0, 0 };
+  tybl::vodka::array::array<double,3> z{ 0, 0, 0 };
 
   CHECK(5.0 == magnitude(a));
   CHECK(1.0 == magnitude(i));
@@ -177,9 +177,9 @@ TEST_CASE("magnitude") {
 }
 
 TEST_CASE("cross product") {
-  tybl::containers::array<double,3> i{ 1, 0, 0 };
-  tybl::containers::array<double,3> j{ 0, 1, 0 };
-  tybl::containers::array<double,3> k{ 0, 0, 1 };
+  tybl::vodka::array::array<double,3> i{ 1, 0, 0 };
+  tybl::vodka::array::array<double,3> j{ 0, 1, 0 };
+  tybl::vodka::array::array<double,3> k{ 0, 0, 1 };
 
   CHECK(k == cross_product(i, j));
   CHECK(-k == cross_product(j, i));
@@ -190,8 +190,8 @@ TEST_CASE("cross product") {
 }
 
 TEST_CASE("Anticommutativity of the cross product") {
-  tybl::containers::array<double,3> a{ 1, 2, 3 };
-  tybl::containers::array<double,3> b{ 4, 5, 6 };
+  tybl::vodka::array::array<double,3> a{ 1, 2, 3 };
+  tybl::vodka::array::array<double,3> b{ 4, 5, 6 };
 
   auto l = cross_product(a, b);
   auto r = cross_product(-b, a);
@@ -200,9 +200,9 @@ TEST_CASE("Anticommutativity of the cross product") {
 }
 
 TEST_CASE("Distributive law for the cross product") {
-  tybl::containers::array<double,3> a{ 1, 2, 3 };
-  tybl::containers::array<double,3> b{ 4, 5, 6 };
-  tybl::containers::array<double,3> c{ 7, 8, 9 };
+  tybl::vodka::array::array<double,3> a{ 1, 2, 3 };
+  tybl::vodka::array::array<double,3> b{ 4, 5, 6 };
+  tybl::vodka::array::array<double,3> c{ 7, 8, 9 };
 
   auto l = cross_product(a, (b + c));
   auto r = cross_product(a, b) + cross_product(a, c);
@@ -211,8 +211,8 @@ TEST_CASE("Distributive law for the cross product") {
 }
 
 TEST_CASE("Scalar factorization for the cross product") {
-  tybl::containers::array<double,3> a{ 1, 2, 3 };
-  tybl::containers::array<double,3> b{ 4, 5, 6 };
+  tybl::vodka::array::array<double,3> a{ 1, 2, 3 };
+  tybl::vodka::array::array<double,3> b{ 4, 5, 6 };
   double t = 9.0;
 
   auto l = cross_product((t * a), b);
@@ -224,9 +224,9 @@ TEST_CASE("Scalar factorization for the cross product") {
 }
 
 TEST_CASE("Vector triple product") {
-  tybl::containers::array<double,3> a{ 1, 2, 3 };
-  tybl::containers::array<double,3> b{ 4, 5, 6 };
-  tybl::containers::array<double,3> c{ 7, 8, 9 };
+  tybl::vodka::array::array<double,3> a{ 1, 2, 3 };
+  tybl::vodka::array::array<double,3> b{ 4, 5, 6 };
+  tybl::vodka::array::array<double,3> c{ 7, 8, 9 };
 
   auto l = cross_product(a, cross_product(b, c));
   auto r = b * dot_product(a, c) - c * dot_product(a, b);
@@ -235,8 +235,8 @@ TEST_CASE("Vector triple product") {
 }
 
 TEST_CASE("Lagrange's identity") {
-  tybl::containers::array<double,3> a{ 1, 2, 3 };
-  tybl::containers::array<double,3> b{ 4, 5, 6 };
+  tybl::vodka::array::array<double,3> a{ 1, 2, 3 };
+  tybl::vodka::array::array<double,3> b{ 4, 5, 6 };
 
   auto tmp = cross_product(a,b);
   auto l = dot_product(tmp, tmp);
@@ -246,9 +246,9 @@ TEST_CASE("Lagrange's identity") {
 }
 
 TEST_CASE("Scalar triple product") {
-  tybl::containers::array<double,3> a{ 1, 2, 3 };
-  tybl::containers::array<double,3> b{ 4, 5, 6 };
-  tybl::containers::array<double,3> c{ 7, 8, 9 };
+  tybl::vodka::array::array<double,3> a{ 1, 2, 3 };
+  tybl::vodka::array::array<double,3> b{ 4, 5, 6 };
+  tybl::vodka::array::array<double,3> c{ 7, 8, 9 };
 
   auto l = dot_product(cross_product(a, b), c);
   auto m = dot_product(cross_product(b, c), a);
@@ -259,8 +259,8 @@ TEST_CASE("Scalar triple product") {
 }
 
 TEST_CASE("Projection/Rejection") {
-  tybl::containers::array<double,3> a{ 1, 2, 3 };
-  tybl::containers::array<double,3> b{ 4, 5, 6 };
+  tybl::vodka::array::array<double,3> a{ 1, 2, 3 };
+  tybl::vodka::array::array<double,3> b{ 4, 5, 6 };
 
   auto p = projection(a,b);
   auto r = rejection(a,b);
